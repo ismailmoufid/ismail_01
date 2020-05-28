6232cb3168508317f87bc787bf93879611f802d3
ismailmoufid <65515013+ismailmoufid@users.noreply.github.com>
2020-05-28 12:04:41 +0100

    Add files via upload

---
+++ my first web page/PHP files/head.php
@@ -0,0 +1,9 @@
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@1,300&display=swap" rel="stylesheet">
+	<link rel="shortcut icon" href="images directory\favicon.ico">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Taïbatoune-3AC</title>
+</head>
---
+++ my first web page/PHP files/nav.php
@@ -0,0 +1,30 @@
+<nav>
+    <div class="logo">
+		<h4>TAïBATOUNE</h4>
+	</div>
+	<ul class="nav-links">
+		<li>
+			<a href="index.html" class="Home">HOME</a>
+		</li>
+        <li>
+        	<a href="learning.html">LEARNING</a>
+        </li>
+		<li>
+			<a href="hobbies.html">HOBBIES</a>
+		</li>
+		<li>
+			<a href="tournaments.html">TOURNAMENTS</a>
+		</li>
+		<li>
+			<a href="about.html">ABOUT</a>
+		</li>
+	</ul>
+	<div class="burger">
+		<div class="line-1">
+		</div>
+		<div class="line-2">
+		</div>
+		<div class="line-3">
+		</div>
+    </div>
+</nav>
